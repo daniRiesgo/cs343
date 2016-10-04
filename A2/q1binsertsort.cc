3a5f2cda984b364728f9e3840da5a60628e5a1b5
Daniel Medina <danielmedinagarcia@protonmail.com>
2016-10-04 19:49:38 -0400

    d

--- A2/q1binsertsort.cc
+++ A2/q1binsertsort.cc
@@ -33,7 +33,6 @@ template<typename T> _Coroutine Binsertsort {
 void uMain::main() {
     int unsorted[] = {25, 6, 9, 5, 99, 100, 101, 7};
     int size = 0;
-    const int Sentinel = 1;
     Binsertsort<int> root = ( Sentinel );
 
     std::cout << "Initial values were: ";
