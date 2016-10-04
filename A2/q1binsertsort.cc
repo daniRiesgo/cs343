0f5af45b402f711ccab01ca58a0651d9ac887bd5
Daniel Medina <danielmedinagarcia@protonmail.com>
2016-10-04 01:30:39 -0400

    bold try

---
+++ A2/q1binsertsort.cc
@@ -0,0 +1,55 @@
+using namespace std;
+
+template<typename T> _Coroutine Binsertsort {
+    const T Sentinel;            // value denoting end of set
+    T value;                     // communication: value being passed down/up the tree
+    void main();                 // YOU WRITE THIS ROUTINE
+  public:
+    Binsertsort( T Sentinel ) : Sentinel( Sentinel ) {}
+
+    void sort( T value ) {       // value to be sorted
+        Binsertsort::value = value;
+        resume();
+    }
+
+    T retrieve() {               // retrieve sorted value
+        resume();
+        return value;
+    }
+
+  private:
+    void main() {
+        Binsertsort less = Binsertsort( Sentinel ), greater = Binsertsort( Sentinel );
+        for ( ;; ) {
+            if ( value < Sentinel ) less.sort( value );
+            else greater.sort( value );
+            resume();
+        }
+
+    }
+};
+
+void uMain::main() {
+    int unsorted[] = {25, 6, 9, 5, 99, 100, 101, 7};
+    int size = 0;
+    const int Sentinel = 1;
+    Binsertsort root = Binsertsort( Sentinel );
+
+    cout << "Initial values were: "
+
+    for ( auto value : unsorted ) {
+        root.sort( value );
+        cout << value;
+        size++;
+    }
+
+    cout << endl << "And sorted values are: ";
+
+    int sorted[size];
+    for ( int i = 0; i < size; i++ ) {
+        sorted[ i ] = root.retrieve();
+        cout << sorted[ i ];
+    }
+
+    cout << endl;   
+}
