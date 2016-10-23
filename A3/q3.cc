25dafb2ea71b60b764519021d74e48dcbbb9c45e
Daniel Medina <danielmedinagarcia@protonmail.com>
2016-10-23 01:11:50 -0400

    testing

--- A3/q3.cc
+++ A3/q3.cc
@@ -4,6 +4,7 @@
 #define BUFFER_SIZE 10
 #define PRODUCE 10
 #define DELAY 10
+#define DEBUGOUTPUT
 
 using namespace std;
 
@@ -18,21 +19,44 @@ template<typename T> class BoundedBuffer {
         if( buffer == nullptr ) {
             cout << "Error allocating buffer. Stop." << endl;
         }
+        #ifdef DEBUGOUTPUT
+            cout << "Buffer created, size " << size << endl;
+        #endif
     }
 
     void insert( T elem ) {
         if( items < size ) {
+            #ifdef DEBUGOUTPUT
+                cout << "Buffer: Acquiring lock" << endl;
+            #endif
             lock.acquire();
+            #ifdef DEBUGOUTPUT
+                cout << "Buffer: inserting in pos " << pos;
+                cout << " value " << elem << endl;
+            #endif
             buffer[ ++pos % size ] = elem;
+            items++;
             lock.release();
+            #ifdef DEBUGOUTPUT
+                cout << "Buffer: Lock released" << endl;
+            #endif
         }
         else _Throw E();
     }
     T remove() {
         if( items > 0 ) {
+            #ifdef DEBUGOUTPUT
+                cout << "Buffer: Adquiring item " << endl;
+            #endif
+            items--;
             return buffer[ pos-- % size ];
         }
-        else _Throw E();
+        else {
+            #ifdef DEBUGOUTPUT
+                cout << "Buffer: Failed to adquire item." << endl;
+            #endif
+            _Throw E();
+        }
     }
     ~BoundedBuffer() {
         free( buffer );
@@ -66,14 +90,23 @@ _Task Producer {
             yield( random() % ( Delay-1 ) );
             // produce corresponding item
             try { buffer.insert( (int) i ); }
-            catch( E ) { i--; }
+            catch( E ) {
+                i--;
+                #ifdef DEBUGOUTPUT
+                    cout << "Producer: No space to insert" << endl;
+                #endif
+            }
         }
         for( ;; ) {
             try {
                 buffer.insert( SENTINEL );
                 break;
             }
-            catch( E ) {  }
+            catch( E ) {
+                #ifdef DEBUGOUTPUT
+                    cout << "Producer: No space to insert" << endl;
+                #endif
+            }
         }
     }
 
@@ -108,10 +141,16 @@ _Task Consumer {
                 int value = buffer.remove();
                 if( value != Sentinel ) {
                     sum += value;
-                    cout << "added value " << value << endl;
+                    #ifdef DEBUGOUTPUT
+                        cout << "Consumer: Added value " << value << endl;
+                    #endif
                 }
                 else break;
-            } catch( E ) {}
+            } catch( E ) {
+                #ifdef DEBUGOUTPUT
+                    cout << "Consumer: No values to get" << endl;
+                #endif
+            }
         }
 
         printf( "This should be 45: %d\n", sum );
