d96c47714c49caab9052e45090d8f3a86112c73f
Daniel Medina <danielmedinagarcia@protonmail.com>
2016-10-24 16:48:37 -0400

    testing

--- A3/bufscript.sh
+++ A3/bufscript.sh
@@ -1,37 +1,37 @@
 # Check bad inputs
-echo "Should warn of bad cons:\n"
+echo "Should warn of bad cons:"
 ./buffer -1
-echo "Should warn of bad prods:\n"
+echo "Should warn of bad prods:"
 ./buffer 1 -2
-echo "Should warn of bad cons:\n"
+echo "Should warn of bad cons:"
 ./buffer 0
-echo "Should warn of bad produce:\n"
+echo "Should warn of bad produce:"
 ./buffer 1 2 0
-echo "Should warn of bad produce:\n"
+echo "Should warn of bad produce:"
 ./buffer 5 3 -1
-echo "Should warn of bad produce:\n"
+echo "Should warn of bad produce:"
 ./buffer 5 3 2147483650
-echo "Should warn of bad bufsize:\n"
+echo "Should warn of bad bufsize:"
 ./buffer 5 3 10 0
-echo "Should warn of bad bufsize:\n"
+echo "Should warn of bad bufsize:"
 ./buffer 5 3 10 -1
-echo "Should warn of bad bufsize:\n"
+echo "Should warn of bad bufsize:"
 ./buffer 5 3 10 100000000000
-echo "Should warn of bad delay:\n"
+echo "Should warn of bad delay:"
 ./buffer 5 3 10 10 -1
-echo "Should warn of bad prods:\n"
+echo "Should warn of bad prods:"
 ./buffer 5 43222222222222
-echo "Should warn of bad prods+cons:\n"
+echo "Should warn of bad prods+cons:"
 ./buffer 2147483640 40
 
 # Check corner cases
-echo "Total should be 55\n"
+echo "Total should be 55"
 ./buffer 1 1
-echo "Total should be 165\n"
+echo "Total should be 165"
 ./buffer
-echo "Total should be 15150\n"
+echo "Total should be 15150"
 ./buffer 5 3 100
-echo "Total should be 165\n"
+echo "Total should be 165"
 ./buffer 10000 3 10
-echo "Total should be 60000\n"
+echo "Total should be 60000"
 ./buffer 1 10000 3 30000
