500c2ab7a56361a1346ab2a27aa4bf09fb39ee91
Daniel Medina <danielmedinagarcia@protonmail.com>
2016-10-25 17:41:41 -0400

    testing

--- A3/q3.cc
+++ A3/q3.cc
@@ -3,7 +3,14 @@
 
 #include "MPRNG.h"
 
-#include "q3.h"
+#define CONSUMERS 5
+#define PRODUCERS 3
+#define PRODUCE 10
+#define BUFFER_SIZE 10
+#ifdef DEBUGOUTPUT
+    #define ERROROUTPUT
+#endif
+#define MAX_INT 2147483647
 
 using namespace std;
 
