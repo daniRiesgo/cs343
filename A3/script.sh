bf647b3f75bf9cb9ec8206090cc3ac8c56d6db66
Daniel Medina <danielmedinagarcia@protonmail.com>
2016-10-25 16:50:13 -0400

    testing

--- A3/script.sh
+++ A3/script.sh
@@ -1,16 +1,23 @@
 echo "student's exec timing"
-time ./matrixmultiply 1 5000 10000 > grep 'user'
-time ./matrixmultiply 2 5000 10000 > grep 'user'
-time ./matrixmultiply 4 5000 10000 > grep 'user'
-time ./matrixmultiply 8 5000 10000 > grep 'user'
-time ./matrixmultiply 16 5000 10000 > grep 'user'
+time ./matrixmultiply 1 5000 10000
+time ./matrixmultiply 2 5000 10000
+time ./matrixmultiply 4 5000 10000
+time ./matrixmultiply 8 5000 10000
+time ./matrixmultiply 16 5000 10000
+
 echo "given exec timing"
-time ./givenmatrixmultiply 1 5000 10000 > grep 'user'
-time ./givenmatrixmultiply 2 5000 10000 > grep 'user'
-time ./givenmatrixmultiply 4 5000 10000 > grep 'user'
-time ./givenmatrixmultiply 8 5000 10000 > grep 'user'
-time ./givenmatrixmultiply 16 5000 10000 > grep 'user'
-# time ./givenmatrixmultiply 8 8 8
+time ./givenmatrixmultiply 1 5000 10000
+time ./givenmatrixmultiply 2 5000 10000
+time ./givenmatrixmultiply 4 5000 10000
+time ./givenmatrixmultiply 8 5000 10000
+time ./givenmatrixmultiply 16 5000 10000
+
+echo "bad inputs"
+./matrixmultiply
+./matrixmultiply cant parse this
+./matrixmultiply -1 2 3
+./matrixmultiply 4 3 2 invalidfile infalidvile
+./matrixmultiply 4 3 1 3x4.mat 4x4.mat toomanyarguments
 
 # time ./matrixmultiply 3 4 4 3x4.mat 4x4.mat
 # time ./givenmatrixmultiply 3 4 4 3x4.mat 4x4.mat
