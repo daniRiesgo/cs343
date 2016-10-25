130fedcd9fcb34804acefe6ae3c15733ad4fc319
Daniel Medina <danielmedinagarcia@protonmail.com>
2016-10-25 16:34:31 -0400

    testing

--- A3/script.sh
+++ A3/script.sh
@@ -1,15 +1,15 @@
 echo "student's exec timing"
-time ./matrixmultiply 1 5000 10000 | grep 'user'
-time ./matrixmultiply 2 5000 10000 | grep 'user'
-time ./matrixmultiply 4 5000 10000 | grep 'user'
-time ./matrixmultiply 8 5000 10000 | grep 'user'
-time ./matrixmultiply 16 5000 10000 | grep 'user'
+time ./matrixmultiply 1 5000 10000 > grep 'user'
+time ./matrixmultiply 2 5000 10000 > grep 'user'
+time ./matrixmultiply 4 5000 10000 > grep 'user'
+time ./matrixmultiply 8 5000 10000 > grep 'user'
+time ./matrixmultiply 16 5000 10000 > grep 'user'
 echo "given exec timing"
-time ./givenmatrixmultiply 1 5000 10000 | grep 'user'
-time ./givenmatrixmultiply 2 5000 10000 | grep 'user'
-time ./givenmatrixmultiply 4 5000 10000 | grep 'user'
-time ./givenmatrixmultiply 8 5000 10000 | grep 'user'
-time ./givenmatrixmultiply 16 5000 10000 | grep 'user'
+time ./givenmatrixmultiply 1 5000 10000 > grep 'user'
+time ./givenmatrixmultiply 2 5000 10000 > grep 'user'
+time ./givenmatrixmultiply 4 5000 10000 > grep 'user'
+time ./givenmatrixmultiply 8 5000 10000 > grep 'user'
+time ./givenmatrixmultiply 16 5000 10000 > grep 'user'
 # time ./givenmatrixmultiply 8 8 8
 
 # time ./matrixmultiply 3 4 4 3x4.mat 4x4.mat
