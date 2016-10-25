7d8f696355744cb29a27e6aa701b73007bf1e536
Daniel Medina <danielmedinagarcia@protonmail.com>
2016-10-25 19:26:27 -0400

    testing

--- A3/q3.h
+++ A3/q3.h
@@ -4,12 +4,62 @@
 
 using namespace std;
 
-
+#ifdef BUSY
 template<typename T> class BoundedBuffer {
   public:
     BoundedBuffer( const unsigned int size )
       :
       front(0), back(0), items(0), size(size)
+    {
+        buffer = ( T* ) malloc( size * sizeof( T ) );
+        if( buffer == nullptr ) {
+            cout << "Error allocating buffer. Stop." << endl;
+        }
+    }
+
+    void insert( T elem ) {
+
+        lock.acquire();
+        while( items == size ) {}// noRoom.wait( lock ); }
+        try {
+            buffer[ back++ % size ] = elem;
+            back = back % size;
+            items++;
+            // noItems.signal();
+
+        } _Finally { lock.release(); }
+    }
+
+    T remove() {
+
+        int res;
+        lock.acquire();
+        while( items == 0 ) { }//noItems.wait( lock ); }
+        try {
+            // When producers finished, return SENTINEL
+            res = buffer[ front++ % size ];
+            front = front % size;
+            items--;
+            // noRoom.signal();
+        } _Finally { lock.release(); }
+
+        return res;
+    }
+    ~BoundedBuffer() { free( buffer ); }
+  private:
+    T *buffer;
+    int front, back, items, size;
+    uOwnerLock lock;
+    uCondLock noItems, noRoom;
+};
+#endif
+
+#ifdef NOBUSY
+template<typename T> class BoundedBuffer {
+  public:
+    BoundedBuffer( const unsigned int size )
+      :
+      front(0), back(0), items(0), size(size), goingToSignal(false)
     {
         buffer = ( T* ) malloc( size * sizeof( T ) );
         if( buffer == nullptr ) {
@@ -29,10 +79,12 @@ template<typename T> class BoundedBuffer {
 
         } _Finally { lock.release(); }
     }
+
     T remove() {
 
         int res;
         lock.acquire();
+        if( goingToSignal )
         while( items == 0 ) { noItems.wait( lock ); }
         try {
             // When producers finished, return SENTINEL
@@ -49,8 +101,10 @@ template<typename T> class BoundedBuffer {
     T *buffer;
     int front, back, items, size;
     uOwnerLock lock;
-    uCondLock noItems, noRoom;
+    uCondLock noItems, noRoom, barging;
+    bool goingToSignal;
 };
+#endif
 
 _Task Producer {
   public:
