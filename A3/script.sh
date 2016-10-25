d5791a811591d5438e3dd6f8097e22f80a141670
Daniel Medina <danielmedinagarcia@protonmail.com>
2016-10-25 16:06:28 -0400

    testing

--- A3/script.sh
+++ A3/script.sh
@@ -3,6 +3,12 @@ time ./matrixmultiply 2 5000 10000
 time ./matrixmultiply 4 5000 10000
 time ./matrixmultiply 8 5000 10000
 time ./matrixmultiply 16 5000 10000
+echo "given"
+time ./givenmatrixmultiply 1 5000 10000
+time ./givenmatrixmultiply 2 5000 10000
+time ./givenmatrixmultiply 4 5000 10000
+time ./givenmatrixmultiply 8 5000 10000
+time ./givenmatrixmultiply 16 5000 10000
 # time ./givenmatrixmultiply 8 8 8
 
 # time ./matrixmultiply 3 4 4 3x4.mat 4x4.mat
