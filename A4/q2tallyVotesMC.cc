3a7e90cc9b54b53734dcfdaccbd07037754f9679
Daniel Medina <danielmedinagarcia@protonmail.com>
2016-11-06 12:43:34 -0500

    q2 setup

--- A4/q2tallyVotesMC.cc
+++ A4/q2tallyVotesMC.cc
@@ -4,13 +4,25 @@ TallyVotes::TallyVotes( unsigned int group, Printer &printer ) {
 
 }
 
-voter::main() {
-   // • yield a random number of times, between 0 and 19 inclusive, so all tasks do not start simultaneously 
+Voter::Voter( unsigned int id, TallyVotes &voteTallier, Printer &printer ) {
+
+}
+
+void Voter::main() {
+   // • yield a random number of times, between 0 and 19 inclusive, so all tasks do not start simultaneously
    // • print start message
    // • yield once using yield( times )
    // • vote (once only)
    // • yield once
    // • print finish message
+}
+
+Tour Voter::vote( unsigned int id, Tour ballot ) {
+    return Tour::Picture;
+}
+
+Printer::Printer( unsigned int voters ) {
+
 }
 
 void Printer::print( unsigned int id, Voter::States state ) {
