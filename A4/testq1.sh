3e6b057a9069672be7c51b6dc560e0f63e816b76
Daniel Medina <danielmedinagarcia@protonmail.com>
2016-11-06 09:34:15 -0500

    script for q1

--- A4/testq1.sh
+++ A4/testq1.sh
@@ -4,9 +4,7 @@ do
     make new KIND=$k
     for t in 1 2 4
     do
-        # for i in {1..4}
-        # do
-            time -f "%Uu %Ss %E" ./new $t 10000000
-        # done
+        time -f "%Uu %Ss %E" ./new $t 10000000
+        time -f "%Uu %Ss %E" ./new $t 10000000
     done
 done
