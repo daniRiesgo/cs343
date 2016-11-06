ae990006b806ad2be40cb46d1d8784da1f8f2511
Daniel Medina <danielmedinagarcia@protonmail.com>
2016-11-06 14:34:41 -0500

    q2 setup

--- A4/q2tallyVotesMC.cc
+++ A4/q2tallyVotesMC.cc
@@ -5,7 +5,7 @@ TallyVotes::TallyVotes( unsigned int group, Printer &printer ) {
 }
 
 Voter::Voter( unsigned int id, TallyVotes &voteTallier, Printer &printer ) {
-
+    cout << 'wow voter ' << id << ' was created' << endl;
 }
 
 void Voter::main() {
@@ -22,7 +22,17 @@ Tour Voter::vote( unsigned int id, Tour ballot ) {
 }
 
 Printer::Printer( unsigned int voters ) {
-
+    for( uint i = 0; i < voters; ++i ) {
+        cout << "Voter" << setw(3) << i;
+    }
+    cout << endl;
+    for( uint i = 0; i < voters; ++i ) {
+        cout << setw(8) << '=======';
+    }
+}
+Printer::~Printer() {
+    cout << '=================' << endl;
+    cout << 'All tours started'
 }
 
 void Printer::print( unsigned int id, Voter::States state ) {
@@ -35,4 +45,24 @@ void Printer::print( unsigned int id, Voter::States state, TallyVotes::Tour vote
 
 void Printer::print( unsigned int id, Voter::States state, unsigned int numBlocked ) {
 
+}
+
+void uMain::main() {
+    uint v, g, seed;
+    checkInput( argv, argc, g, v, seed );
+
+    Printer p( v );
+    TallyVotes tb( g, p );
+    Voter *voters[v];
+    Tour ballot;
+
+    for( size_t i; i < v; ++i ) {
+        ballot = MPRNG( seed )() % 2 ? Tour::Picture : Tour::Statue;
+        voters[i] = new Voter( i, ballot );
+    }
+
+    for( size_t i; i < v; ++i ) { delete voters[i]; }
+
+    delete tb;
+    delete p;
 }
