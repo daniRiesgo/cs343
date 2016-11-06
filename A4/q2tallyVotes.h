d21162c1d70488f1287251aad3677360a34d6df7
Daniel Medina <danielmedinagarcia@protonmail.com>
2016-11-06 18:42:26 -0500

    q2 setup

--- A4/q2tallyVotes.h
+++ A4/q2tallyVotes.h
@@ -19,19 +19,21 @@ _Cormonitor TallyVotes : public uBarrier {
     #error unsupported voter type
 #endif
     // common declarations
+    Printer &printer;
     void main() {};
   public:                             // common interface
-    TallyVotes( unsigned int group, _Monitor &printer );
+    TallyVotes( unsigned int group, Printer &printer );
     enum Tour { Picture, Statue };
     Tour vote( unsigned int id, Tour ballot );
 };
 
 _Task Voter {
+    Printer &printer;
     void main() {};
   public:
     enum States { Start = 'S', Vote = 'V', Block = 'B', Unblock = 'U', Barging = 'b',
                   Complete = 'C', Finished = 'F' };
-    Voter( unsigned int id, TallyVotes &voteTallier, _Monitor &printer );
+    Voter( unsigned int id, TallyVotes &voteTallier, Printer &printer );
 };
 
 _Monitor Printer {      // chose one of the two kinds of type constructor
