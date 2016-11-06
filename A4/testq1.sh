b67d7519aea7b542a75c9dcf9f9eab5ab6fba4d5
Daniel Medina <danielmedinagarcia@protonmail.com>
2016-11-06 09:08:28 -0500

    script for q1

---
+++ A4/testq1.sh
@@ -0,0 +1,11 @@
+for k in "STACK" "VECTOR2" "VECTOR1" "DARRAY"
+do
+    make new KIND=$k
+    for t in 1 2 4
+    do
+        for i in {1..4}
+        do
+            time -f "%Uu %Ss %E" ./new $t 10000000
+        done
+    done
+done
