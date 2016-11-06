19ca1b52bd7186b7b37983b7c4141308793b7f0a
Daniel Medina <danielmedinagarcia@protonmail.com>
2016-11-06 09:29:44 -0500

    script for q1

--- A4/testq1.sh
+++ A4/testq1.sh
@@ -1,15 +1,12 @@
-nano time_results
+
 for k in "STACK" "VECTOR2" "VECTOR1" "DARRAY"
 do
     make new KIND=$k
-    echo "Version: $k\n\n" > time_results
     for t in 1 2 4
     do
-        echo "With $t task:\n" > time_results
-        for i in {1..4}
-        do
-            time -f "%Uu %Ss %E" ./new $t 10000000 > time_results
-        done
+        # for i in {1..4}
+        # do
+            time -f "%Uu %Ss %E" ./new $t 10000000
+        # done
     done
-    echo "\n\n" > time_results
 done
