f07b3f295a8cdceb89920bc1675cc3281aea7048
Daniel Medina <danielmedinagarcia@protonmail.com>
2016-11-07 02:25:14 -0500

    q2 setup

--- A4/q2tallyVotesMC.cc
+++ A4/q2tallyVotesMC.cc
@@ -123,6 +123,10 @@ void uMain::main() {
         cout << "Voters initialized" << endl;
         #endif
 
+        #ifdef __U_MULTI__
+        uProcessor p[3] __attribute__ (( unused )); // create 3 kernel thread for a total of 4
+        #endif
+
         for( uint i = 0; i < v; ++i ) {
             voters[i] = new Voter( i, tb, p );
             #ifdef VERBOSE
