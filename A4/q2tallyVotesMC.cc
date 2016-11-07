d6bfeac8f197de7957c769de5953ad0362e7a080
Daniel Medina <danielmedinagarcia@protonmail.com>
2016-11-07 04:00:38 -0500

    q2 setup

--- A4/q2tallyVotesMC.cc
+++ A4/q2tallyVotesMC.cc
@@ -81,7 +81,7 @@ TallyVotes::Tour TallyVotes::vote( unsigned int id, TallyVotes::Tour ballot ) {
 void Printer::main() {
     for( uint i = 0; i < voters; ++i ) {
         string out = "Voter";
-        out += itoa(i);
+        out += to_string(i);
         int spaces = 8-out.length();
         for(int i = 0; i<spaces; ++i) out += " ";
         cout << out;
