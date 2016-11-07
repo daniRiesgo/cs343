b81d99155921011be353f58d8efb3e173b958814
Daniel Medina <danielmedinagarcia@protonmail.com>
2016-11-07 05:09:05 -0500

    q2 setup

--- A4/q2tallyVotesMC.cc
+++ A4/q2tallyVotesMC.cc
@@ -8,7 +8,7 @@ using namespace std;
 bool checkInput( char *argv[], const int argc, uint & g, uint & v, uint & seed ) ;
 
 uint seed = 0;
-MPRNG rand = new MPRNG();
+MPRNG *rand = new MPRNG();
 
 void Voter::main() {
 
