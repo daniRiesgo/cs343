3f925c36dd121c18dc721deeb58aa72e41b9962c
Daniel Medina <danielmedinagarcia@protonmail.com>
2016-11-19 13:46:55 -0500

    INTB done. AUTO try

--- A5/AutomaticSignal.h
+++ A5/AutomaticSignal.h
@@ -1,3 +1,3 @@
 #define AUTOMATIC_SIGNAL uCondition cond;
 #define WAITUNTIL( pred, before, after ) before; while( !pred ) cond.wait(); after; cond.signal();
-#define RETURN( expr... ) cond.signal(); return __VA_ARGS__;
+#define RETURN( expr... ) cond.signal(); return expr...;
